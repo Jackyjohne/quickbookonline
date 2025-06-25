e42c355ad76a65f1f6aecd4323cd357285f17513
Jackyjohne <Jackyjohne@outlook.com>
2025-06-25 12:46:37 +0530

    Update livechat.js

--- docs/_static/livechat.js
+++ docs/_static/livechat.js
@@ -1,14 +1,6 @@
-project = 'quickbookonline'
-author = 'Your Name'
-release = '1.0'
-
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-html_js_files = [
-    'livechat.js',
-]
+document.addEventListener("DOMContentLoaded", function () {
+    var ze = document.createElement('script');
+    ze.id = 'ze-snippet';
+    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+    document.body.appendChild(ze);
+});
